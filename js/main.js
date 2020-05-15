909c79afbd186acb1f36bc8b104432ba0d31b7ad
increase21 <engr.increase@gmail.com>
2020-05-15 10:28:30 +0100

    addition of correct endpoint

--- js/main.js
+++ js/main.js
@@ -58,16 +58,20 @@ $(document).ready(() => {
       }
       // check phone
       if (phone === "" || isNaN(phone) || phone.length > 10) {
-         return errorDiv.text("A valid phone number is required");
+         return errorDiv.text("A valid 10 digits phone number is required");
       }
       // check zip
       if (zip === "" || isNaN(zip) || zip.length > 6) {
          return errorDiv.text("A zip code is required");
       }
       $('#submit_btn_click').text('Loading...').prop('disabled', true)
-      let URL = `https://gratisdigital.listflex.com/lmadmin/api/leadimport.php?apikey=TSKQA35V04N9MD6GY4D7&list_id=1692&fname=${fname}&lname=${lname}&email=${email}&phone=${phone}&address=${addr}&zip=${zip}&country=US`
-      $.get(URL).done(e => {
-         if (e === 'Success') {
+      // let URL = `https://gratisdigital.listflex.com/lmadmin/api/leadimport.php?apikey=TSKQA35V04N9MD6GY4D7&list_id=1692&fname=${fname}&lname=${lname}&email=${email}&phone=${phone}&address=${addr}&zip=${zip}&country=US`
+      $.ajax({
+         method: "POST",
+         url: "https://hooks.zapier.com/hooks/catch/2128931/oi3swwa",
+         data: $('#lead_form_fr').serialize()
+      }).done(e => {
+         if (e.status === 'success') {
             $('#modal-container').html(`<div class="p-5 text-center">
                <h4 class="text-green">Thank You! We will contact you shortly!</h4>
                <button type="button" class="btn mt-3 btn-light w-100 close-modal" id="">Cancel</button></div>
